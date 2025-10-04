7c603869ce011fe4cfd11786a2873bda4e2b9d2a
turbokuzmich <kurteev.d@yandex.ru>
2025-10-04 13:15:44 +0300

    note about postcss-clampwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,13 @@
+/**
+ * Important note about postcss-clampwind!
+ *
+ * In order to make clampwind work, you should hack
+ * /node_modules/postcss-clampwind/dist/clampwind.cjs.cjs
+ * file and add the following code at the end of the file:
+ *
+ * module.exports = clampwind_default;
+ */
+
 const config = {
   plugins: ["@tailwindcss/postcss", "postcss-clampwind"],
 };
